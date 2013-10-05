1724951c22b9738285e82585b3b30908b4e7f90d
Vince Allen <vince@foldifoldi.com>
2013-10-05 18:34:11 -0400

    Updates render script.

--- main.js
+++ main.js
@@ -5,16 +5,7 @@
   var generator;
   var data;
   var fs = require('fs');
-  var file = __dirname + '/data.json';
-   
-  fs.readFile(file, 'utf8', function (err, data) {
-    if (err) {
-      console.log('Error: ' + err);
-      return;
-    }
-    data = JSON.parse(data);
-    console.dir(data);
-  });
+  var file = __dirname + '/agent_data.json';
 
   function init(gen) {
     generator = gen;
@@ -24,121 +15,141 @@
   }
 
   function menuClicked(e) {
+
     if (e.generatorMenuChanged.name === 'fp') {
 
-      var getInitialPrefs = "var startTypeUnits = app.preferences.typeUnits;";
+      fs.readFile(file, 'utf8', function (err, data) {
+        if (err) {
+          console.log('Error: ' + err);
+          return;
+        }
 
-      var setPrefs = "app.preferences.rulerUnits = Units.PIXELS;";
+        var retina = 2;
 
-      var restorePrefs = "app.preferences.rulerUnits = startTypeUnits;";
+        var map = "map = function(value, min1, max1, min2, max2) { \
+            var unitratio = (value - min1) / (max1 - min1); \
+            return (unitratio * (max2 - min2)) + min2; \
+        };"
 
-      var frames = "var frames = [\
-          {\
-            world: { \
-              width: 800, \
-              height: 600, \
-              resolution: 4, \
-              colorMode: 'hsla' \
-            }, \
-            items: [ \
-              { \
-                width: 20, \
-                height: 20, \
-                scale: 0.866, \
-                location: { \
-                  x: 75.3514, \
-                  y: 58.8532 \
-                }, \
-                velocity: { \
-                  x: 0.1586, \
-                  y: -0.6329 \
-                }, \
-                hue: 13, \
-                saturation: 50, \
-                lightness: 50, \
-                color: [215, 140, 86], \
-                opacity: 0.4893 \
+        var constrain = "var constrain = function(val, low, high) { \
+          if (val > high) { \
+            return high; \
+          } else if (val < low) { \
+            return low; \
+          } \
+          return val; \
+        };";
+
+        var mag = "var mag = function(x, y) {return Math.sqrt((x * x) + (y * y));};";
+
+        var radiansToDegrees = "var radiansToDegrees = function(radians) {return radians * (180/Math.PI);};";
+
+        var isInside = "var isInside = function(obj, container) { \
+            if (obj.location.x + obj.width/2 > container.location.x - container.width/2 && \
+              obj.location.x - obj.width/2 < container.location.x + container.width/2 && \
+              obj.location.y + obj.height/2 > container.location.y - container.height/2 && \
+              obj.location.y - obj.height/2 < container.location.y + container.height/2) { \
+              return true; \
+            } \
+            return false; \
+          };";
+
+        var getInitialPrefs = "var startTypeUnits = app.preferences.typeUnits;";
+
+        var setPrefs = "app.preferences.rulerUnits = Units.PIXELS;";
+
+        var restorePrefs = "app.preferences.rulerUnits = startTypeUnits;";
+
+        var frames = "var frames = " + data + ";";
+
+        var openFrameLoop = "for(var i = 0, max = frames.length; i < max; i++) {";
+
+        var createDoc = "var docWidth = frames[i].world.width * frames[i].world.resolution * " + retina + "; \
+            var docHeight = frames[i].world.height * frames[i].world.resolution * " + retina + "; \
+            app.documents.add(docWidth, docHeight, 144, 'docRef' + i, NewDocumentMode.RGB);";
+
+        var fillBackground = "var solidColor = new SolidColor(); \
+            solidColor.rgb.red = 0; \
+            solidColor.rgb.green = 0; \
+            solidColor.rgb.blue = 0; \
+            app.activeDocument.selection.selectAll(); \
+            app.activeDocument.selection.fill(solidColor); \
+            app.activeDocument.selection.deselect();";
+
+        var openMainLoop = "for(var j = 0; j < frames[i].items.length; j++) {";
+
+        var main = "var item = frames[i].items[j]; \
+            app.activeDocument.artLayers.add(); \
+            var itemWidth = item.width * item.scale * frames[i].world.resolution; \
+            var itemHeight = item.height * item.scale * frames[i].world.resolution; \
+            var selRegion = Array(Array(0, 0), Array(itemWidth, 0), Array(itemWidth, itemHeight), Array(0, itemHeight)); \
+            app.activeDocument.selection.select(selRegion); \
+            var x = (item.location.x * frames[i].world.resolution * " + retina + ") - itemWidth / 2; \
+            var y = (item.location.y * frames[i].world.resolution * " + retina + ") - itemHeight / 2; \
+            var pos = { \
+              location: { \
+                x: x, \
+                y: y \
               }, \
-              { \
-                width: 40, \
-                height: 40, \
-                scale: 1.166, \
-                location: { \
-                  x: 35.3514, \
-                  y: 98.8532 \
-                }, \
-                velocity: { \
-                  x: 0.1586, \
-                  y: -0.6329 \
-                }, \
-                hue: 130, \
-                saturation: 50, \
-                lightness: 50, \
-                color: [215, 140, 86], \
-                opacity: 0.8893 \
-              }\
-            ]\
-          }\
-        ];";
+              width: itemWidth, \
+              height: itemHeight \
+            }; \
+            var container = { \
+             location: { \
+                x: docWidth / 2, \
+                y: docHeight / 2 \
+              }, \
+              width: docWidth, \
+              height: docHeight \
+            }; \
+            if (!isInside(pos, container)) { \
+              break; \
+            } \
+            app.activeDocument.selection.translateBoundary(x, y); \
+            app.foregroundColor.hsb.hue = item.hue; \
+            app.foregroundColor.hsb.saturation = item.saturation * 100; \
+            app.foregroundColor.hsb.brightness = item.lightness * 100; \
+            app.activeDocument.selection.fill(app.foregroundColor); \
+            app.activeDocument.activeLayer.opacity = constrain(item.opacity * 100, 0, 100); \
+            app.activeDocument.selection.deselect(); \
+            var blurAngle = item.angle; \
+            app.activeDocument.activeLayer.applyMotionBlur(blurAngle, map(mag(item.velocity.x, item.velocity.y), 0, item.maxSpeed, 0, 30));";
 
-      var openFrameLoop = "for(var i = 0, max = frames.length; i < max; i++) {";
+        var closeMainLoop = "}";
 
-      var createDoc = "var docWidth = frames[i].world.width, docHeight = frames[i].world.height; \
-          app.documents.add(docWidth, docHeight, 72, 'docRef', NewDocumentMode.RGB);";
+        var saveFile = "var saveFile = new File('" + __dirname + "/Frames/' + i + '.jpg'); \
+            var saveOptions = new JPEGSaveOptions(); \
+            saveOptions.embedColorProfile = false; \
+            saveOptions.formatOptions = FormatOptions.STANDARDBASELINE; \
+            saveOptions.matte = MatteType.NONE; \
+            saveOptions.quality = 10; \
+            app.activeDocument.saveAs(saveFile, saveOptions, true, Extension.LOWERCASE); \
+            app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);";
 
-      var fillBackground = "var solidColor = new SolidColor(); \
-          solidColor.rgb.red = 0; \
-          solidColor.rgb.green = 0; \
-          solidColor.rgb.blue = 0; \
-          app.activeDocument.selection.selectAll(); \
-          app.activeDocument.selection.fill(solidColor); \
-          app.activeDocument.selection.deselect();";
+        var closeFrameLoop = "}";
 
-      var openMainLoop = "for(var j = 0; j < frames[i].items.length; j++) {";
-
-      var main = "var item = frames[i].items[j]; \
-          app.activeDocument.artLayers.add(); \
-          var itemWidth = item.width * item.scale; \
-          var itemHeight = item.height * item.scale; \
-          var selRegion = Array(Array(0, 0), Array(itemWidth, 0), Array(itemWidth, itemHeight), Array(0, itemHeight)); \
-          app.activeDocument.selection.select(selRegion); \
-          app.activeDocument.selection.translateBoundary(item.location.x * frames[i].world.resolution, item.location.y * frames[i].world.resolution); \
-          app.foregroundColor.hsb.hue = item.hue; \
-          app.foregroundColor.hsb.saturation = item.saturation; \
-          app.foregroundColor.hsb.brightness = item.lightness; \
-          app.activeDocument.selection.fill(app.foregroundColor); \
-          app.activeDocument.activeLayer.opacity = item.opacity * 100; \
-          app.activeDocument.selection.deselect(); \
-          app.activeDocument.activeLayer.applyMotionBlur(30, 10);";
-
-      var closeMainLoop = "}";
-
-      var saveFile = "var saveFile = new File('~/Desktop/Hello' + i + '.jpg'); \
-          saveOptions = new JPEGSaveOptions(); \
-          saveOptions.embedColorProfile = false; \
-          saveOptions.formatOptions = FormatOptions.STANDARDBASELINE; \
-          saveOptions.matte = MatteType.NONE; \
-          saveOptions.quality = 10; \
-          app.activeDocument.saveAs(saveFile, saveOptions, true, Extension.LOWERCASE);";
-
-      var closeFrameLoop = "}";
-
-      generator.evaluateJSXString(getInitialPrefs +
-          setPrefs +
-          frames +
-          openFrameLoop +
-          createDoc +
-          fillBackground +
-          openMainLoop +
-          main +
-          closeMainLoop +
-          saveFile +
-          closeFrameLoop +
-          restorePrefs);
+        generator.evaluateJSXString(map +
+            constrain +
+            mag +
+            radiansToDegrees +
+            isInside +
+            getInitialPrefs +
+            setPrefs +
+            frames +
+            openFrameLoop +
+            createDoc +
+            fillBackground +
+            openMainLoop +
+            main +
+            closeMainLoop +
+            saveFile +
+            closeFrameLoop +
+            restorePrefs);
+      });
     }
   }
 
-  /* 
+  /*
    * update width/height based on scale; ex: var itemWidth = data.width * data.scale; itemHeight = data.height * data.scale;
    * create selection based on width, height; ex: var selRegion = Array(Array(0, 0), Array(itemWidth, 0), Array(itemWidth, itemHeight), Array(0, itemHeight));
    * translate selection based on location.x, location.y; ex: selRegion.translateBoundary(data.location.x * data.world.resolution, data.location.y * data.world.resolution)
@@ -149,6 +160,7 @@
    * set layer opacity based on opacity; ex: app.activeDocument.activeLayer.opacity = [0.0..100.0];
    * deselect
    * apply motion blur based on item's angle (direction) and velocity; ex: app.activeDocument.activeLayer.applyMotionBlur(30, 10);
+   * motion blur wants a number bw -90 & 90
    * saveFile
    */
 
