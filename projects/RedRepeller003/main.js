52638bf60b72a5385e47549cb411c5b149119059
Vince Allen <vince@foldifoldi.com>
2013-11-23 09:36:45 -0500

    Adds bit-gen-utils.

--- projects/RedRepeller003/main.js
+++ projects/RedRepeller003/main.js
@@ -3,25 +3,32 @@
  'use strict';
   console.log('Version: ' + process.version);
   var sys = require('sys'),
-      exec = require('child_process').exec,
       fs = require('fs'),
       util = require('util'),
       http = require('http'),
       https = require('https'),
-      restclient = require('node-restclient'),
-      OAuth = require('oauth');
+      RestClient = require('node-rest-client').Client,
+      OAuth = require('oauth'),
+      getFolderName = require('./bit-gen-utils').getFolderName,
+      msToSec = require('./bit-gen-utils').msToSec,
+      msToMin = require('./bit-gen-utils').msToMin;
 
-  var bossServer = 'http://bit-shadow-boss.jit.su';
+  var bossServer = 'http://bit-shadow-bouncer.jit.su';
+
+  var restClient = new RestClient();
+  restClient.on('error', function(err){ // handling client error events
+    console.error('Something went wrong on the client', err);
+  });
 
   var generator;
   var dataFiles = null;
-  var currentFrame = 100; // boss determines current frame; in devMode, we manually increment after frame is done
+  var currentFrame = 0; // boss determines current frame; in devMode, we manually increment after frame is done
   var projectStart = null;
   var framesFolder = null;
   var config = null;
   var sendTweet = false;
-  var framesBTWTweets = 100;
-  var devMode = true;
+  var framesBTWTweets = 1;
+  var devMode = false; // if true, reads local data files
   var totalFramesRendered = 0;
 
   function init(gen) {
@@ -55,10 +62,8 @@
         if (devMode) {
           readLocalFile();
         } else {
-          readFile();
+          readRemoteFile();
         }
-
-        //render();
       });
 
     }
@@ -70,16 +75,44 @@
   function readLocalFile() {
 
     var frameStart = new Date().getTime(),
-        file = __dirname + '/data/frame' + currentFrame + '.json',
-        frames = [];
+        file = __dirname + '/data/frame' + currentFrame + '.json';
 
     fs.readFile(file, 'utf8', function (err, data) {
       if (err) {
-        console.log('Error: ' + err);
+        console.log('readLocalFile() Error: ' + err);
         return;
       }
-      frames.push(data);
-      render(data, frameStart); // need to stringify everything
+      render(data, frameStart);
+    });
+  }
+
+  /**
+   * Requests data from the bit-shadow-boss and passes it to render().
+   */
+  function readRemoteFile() {
+
+    var frameStart = new Date().getTime();
+
+    http.get(bossServer + "/data", function(res) {
+
+      var data = '';
+
+      res.on('data', function (chunk){
+          data += chunk;
+      });
+
+      res.on('end', function(){
+        var parsedData = JSON.parse(data);
+        if (parsedData.stop) {
+          return;
+        }
+        currentFrame = parsedData.frame;
+        render(data, frameStart);
+      })
+
+    }).on('error', function(e) {
+      console.log("readRemoteFile() Error: " + e.message + "; trying again...");
+      setTimeout(readRemoteFile, 3000);
     });
   }
 
@@ -139,18 +172,15 @@
 
     //
 
-    var affectLayerSets = false;
+    var affectLayerSets = true;
 
-    // getInitialPrefs
-    var startTypeUnits = app.preferences.typeUnits;
-    // setPrefs
-    app.preferences.rulerUnits = Units.PIXELS;
-    // setDialogMode
-    app.displayDialogs = DialogModes.NO;
+    var startTypeUnits = app.preferences.typeUnits; // getInitialPrefs
+    app.preferences.rulerUnits = Units.PIXELS; // setPrefs
+    app.displayDialogs = DialogModes.NO; // setDialogMode
 
     // setup the initial document
-    var docWidth = 1920;
-    var docHeight = 1080;
+    var docWidth = world.width * world.resolution * 2;
+    var docHeight = world.height * world.resolution * 2;
     app.documents.add(docWidth, docHeight, 144, 'docRef', NewDocumentMode.RGB);
 
     // fill the background
@@ -212,14 +242,14 @@
       app.activeDocument.selection.fill(app.foregroundColor);
       app.activeDocument.selection.translate(docWidth / 2, docHeight / 2);
       app.activeDocument.selection.rotate(item.angle, AnchorPosition.MIDDLECENTER);
+      app.activeDocument.selection.deselect();
       app.activeDocument.activeLayer.opacity = constrain(item.opacity * 100, 0, 100);
       var blurAngle = constrain(item.angle, -360, 360);
       var blurDistance = constrain(map(mag(item.velocity.x, item.velocity.y), 0, item.maxSpeed, 0, 30), 1, 2000);
       app.activeDocument.activeLayer.applyMotionBlur(blurAngle, blurDistance);
-      app.activeDocument.selection.translate(x - (docWidth / 2), y - (docHeight / 2));
+      app.activeDocument.activeLayer.translate(x - (docWidth / 2), y - (docHeight / 2));
 
       //
-      app.activeDocument.selection.deselect();
 
       /*
        * If the total number of art layers equals maxLayersPerSet, merge the current set
@@ -227,7 +257,7 @@
        */
       if (myLayerSets[myLayerSets.length - 1].artLayers.length >= maxLayersPerSet) {
         if (affectLayerSets) { // check if we need to affect layer set
-          //layerSetsEffect(i, myLayerSets);
+          layerSetsEffect(i, myLayerSets);
         } else {
           myLayerSets[myLayerSets.length - 1].merge();
         }
@@ -263,9 +293,11 @@
 
     generator.evaluateJSXString(str.slice(0, str.length - 3)).done(
         function (document) {
-          console.log('done!');
 
           var frameDuration = new Date().getTime() - frameStart;
+          var dur = frameDuration < 60000 ? msToSec(frameDuration) + ' seconds' : msToMin(frameDuration) + ' minutes';
+          console.log('Rendered frame ' + currentFrame + ' in ' + dur + '.');
+
           totalFramesRendered++;
 
           if (!devMode) {
@@ -275,14 +307,164 @@
             readLocalFile();
           }
 
-          /*var projectTime = msToSec(new Date().getTime() - projectStart);
+          // check to send Tweet
           if (sendTweet && !(totalFramesRendered % framesBTWTweets)) {
-            createTweetStatus(projectTime);
-          }*/
+            createTweetStatus(frameDuration, createTweet, function() {
+              if (devMode) {
+                readLocalFile();
+              } else {
+                readRemoteFile();
+              }
+            });
+          }
         },
         function (err) {
             console.error('err: ', err);
         });
+  }
+
+  /**
+   * Queries Wordnik for random words and concatenates a phrase. If the query is successful,
+   * calls createTweet to post the status to Twitter.
+   *
+   * @param {number} frameDuration The time in seconds it took to render the frame.
+   */
+  function createTweetStatus(frameDuration, callback, errback) {
+
+    var getNounsURL = "http://api.wordnik.com/v4/words.json/randomWords?" +
+      "minCorpusCount=1000" +
+      "&minDictionaryCount=10" +
+      "&excludePartOfSpeech=noun,verb,adverb,interjection,pronoun,preposition,abbreviation,affix,article,auxiliary-verb,conjunction,definite-article,family-name,given-name,idiom,imperative,noun-plural,past-participle,proper-noun,proper-noun-plural,suffix,verb-intransitive,verb-transitive" + // noun-possessive, phrasal-prefix
+      "&includePartOfSpeech=adjective" +
+      "&hasDictionaryDef=true" +
+      "&limit=5" +
+      "&maxLength=12" +
+      "&api_key=" + config.wordnik_api_key;
+
+    restClient.get(getNounsURL, function(data, response) {
+
+      var results = JSON.parse(data), // parsed response body as js object
+          subject = results[0].word,
+          modifier = results[1].word,
+          dur = frameDuration < 60000 ? msToSec(frameDuration) + ' seconds' : msToMin(frameDuration) + ' minutes',
+          status = 'The ' + subject + ' is ' + modifier + '! Rendered frame ' + currentFrame + ' in ' + dur + ' secs.';
+
+      createTweet.call(null, status);
+
+    }).on('error',function(err){
+      console.log('Error: something went wrong on the wordnik request', err.request.options);
+      errback();
+    });
+  }
+
+  function createTweet(status) {
+console.log(status);
+return;
+    var fileName = framesFolder + '/' + (currentFrame - 1) + '.jpg';
+
+    var data = fs.readFileSync(fileName);
+
+    var oauth = new OAuth.OAuth(
+      'https://api.twitter.com/oauth/request_token',
+      'https://api.twitter.com/oauth/access_token',
+      config.twitter_consumer_key,
+      config.twitter_consumer_secret,
+      '1.0A',
+      null,
+      'HMAC-SHA1'
+    );
+
+    uploadMedia(oauth, status, fileName);
+
+    // uncomment to just tweet a status without media
+    // function(url, oauth_token, oauth_token_secret, post_body, post_content_type, callback)
+    /*oauth.post(
+      'https://api.twitter.com/1.1/statuses/update.json',
+      config.twitter_access_token,
+      config.twitter_access_token_secret,
+      {status: status},
+      'application/json',
+      function (err, data, res){
+        if (err) {
+          console.error(err);
+        }
+        console.log(util.inspect(data));
+      });*/
+ }
+
+  /**
+   * Manually builds a multipart/form-data request and makes the post.
+   *
+   * http://stackoverflow.com/questions/12921371/posting-images-to-twitter-in-node-js-using-oauth
+   */
+  function uploadMedia(oauth, status, fileName) {
+
+    var data = fs.readFileSync(fileName);
+    var crlf ='\r\n';
+    var boundary = '---------------------------10102754414578508781458777923';
+
+    var separator = '--' + boundary;
+    var footer = crlf + separator + '--' + crlf;
+    var fileHeader = 'Content-Disposition: file; name="media"; filename="' + fileName + '"';
+
+    var contents = separator + crlf
+        + 'Content-Disposition: form-data; name="status"' + crlf
+        + crlf
+        + status + crlf
+        + separator + crlf
+        + fileHeader + crlf
+        + 'Content-Type: image/jpeg' +  crlf
+        + crlf;
+
+    var multipartBody = Buffer.concat([
+        new Buffer(contents),
+        data,
+        new Buffer(footer)]);
+
+    var hostname = 'api.twitter.com';
+    var authorization = oauth.authHeader(
+        'https://api.twitter.com/1.1/statuses/update_with_media.json',
+        config.twitter_access_token, config.twitter_access_token_secret, 'POST');
+
+    var headers = {
+        'Authorization': authorization,
+        'Content-Type': 'multipart/form-data; boundary=' + boundary,
+        'Host': hostname,
+        'Content-Length': multipartBody.length,
+        'Connection': 'Keep-Alive'
+    };
+
+    var options = {
+        host: hostname,
+        port: 443,
+        path: '/1.1/statuses/update_with_media.json',
+        method: 'POST',
+        headers: headers
+    };
+
+    var request = https.request(options);
+    request.write(multipartBody);
+    request.end();
+
+    request.on('error', function (err) {
+      console.log('Error: Something is wrong.\n' + JSON.stringify(err) + '\n');
+      if (devMode) {
+        readLocalFile();
+      } else {
+        readRemoteFile();
+      }
+    });
+
+    request.on('response', function (response) {
+        response.setEncoding('utf8');
+        response.on('data', function (chunk) {
+            console.log(chunk.toString());
+        });
+        response.on('end', function () {
+            console.log(response.statusCode +'\n');
+        });
+    });
+
   }
 
   /**
@@ -308,27 +490,6 @@
       console.log("Got error: " + e.message);
       readFile();
     });
-  }
-
-  /**
-   * Formats a folder name based on the currrent time.
-   */
-  function getFolderName() {
-
-    var date = new Date(),
-        year = date.getFullYear(),
-        month = date.getMonth(),
-        day = date.getDate(),
-        hours = date.getHours(),
-        minutes = date.getMinutes(),
-        seconds = date.getSeconds();
-
-    return year.toString() +
-        (month < 10 ? '0' + month : month).toString() +
-        (day < 10 ? '0' + day : day).toString() +
-        (hours < 10 ? '0' + hours : hours).toString() +
-        (minutes < 10 ? '0' + minutes : minutes).toString() +
-        (seconds < 10 ? '0' + seconds : seconds).toString();
   }
 
   exports.init = init;
